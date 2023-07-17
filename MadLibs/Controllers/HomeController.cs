fceaba19b7ca4cbeb23a8924abba4437519bfadc
Jason E. Church <elijahchurch@gmail.com>
2023-07-17 13:17:51 -0700

    update story route to access Words

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -9,10 +9,14 @@ namespace MadLibs.Controllers
         public ActionResult Form() {return View();}
 
         [Route("/story")]
-        public ActionResult Story()
+        public ActionResult Story(string name1, string name2, string animal, string exclamation, string thing, string food)
         {
             Words wordList = new Words;
-
+            wordList.Name1 = name1;
+            wordList.Name2 = name2;
+            wordList.Animal = animal;
+            wordList.Exclamation = thing;
+            wordList.Food = food;
             return View(wordList);
         }
     }
