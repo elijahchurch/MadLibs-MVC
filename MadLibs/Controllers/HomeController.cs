6ff3227ec7656db4a1e9f8d41de8097d00bd1b2f
Jason E. Church <elijahchurch@gmail.com>
2023-07-17 13:46:26 -0700

    update story route so all fields get correctly stored

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,21 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using MadLibs.Models;
 
-namespace MadLibs.Controllers 
+namespace MadLibs.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
         [Route("/")]
-        public ActionResult Form() {return View();}
+        public ActionResult Form() { return View(); }
 
         [Route("/story")]
         public ActionResult Story(string name1, string name2, string animal, string exclamation, string thing, string food)
         {
-            Words wordList = new Words;
+            Words wordList = new Words();
             wordList.Name1 = name1;
             wordList.Name2 = name2;
             wordList.Animal = animal;
-            wordList.Exclamation = thing;
+            wordList.Exclamation = exclamation;
+            wordList.Thing = thing;
             wordList.Food = food;
             return View(wordList);
         }
